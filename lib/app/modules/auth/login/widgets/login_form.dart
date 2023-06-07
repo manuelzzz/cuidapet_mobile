f63c642d819a4c0155cbdc88b63043b0f436403c
manel <manuelss.23.bil@gmail.com>
2023-06-07 17:33:23 -0300

    Login page widgets implementation

---
+++ lib/app/modules/auth/login/widgets/login_form.dart
@@ -0,0 +1,64 @@
+part of '../login_page.dart';
+
+class _LoginForm extends StatefulWidget {
+  const _LoginForm();
+
+  @override
+  State<_LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<_LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          CuidapetTextformField(
+            label: 'login',
+          ),
+          const SizedBox(height: 20),
+          CuidapetTextformField(
+            label: 'senha',
+            obscureText: true,
+          ),
+          const _OrSeparator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _OrSeparator extends StatelessWidget {
+  const _OrSeparator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Divider(
+            thickness: 1,
+            color: context.primaryColor,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'ou',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.sp,
+              color: context.primaryColor,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Divider(
+            thickness: 1,
+            color: context.primaryColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/app/modules/auth/login/widgets/login_register_buttons.dart
@@ -0,0 +1,43 @@
+part of '../login_page.dart';
+
+class _LoginRegisterButtons extends StatefulWidget {
+  const _LoginRegisterButtons();
+
+  @override
+  State<_LoginRegisterButtons> createState() => __LoginRegisterButtonsState();
+}
+
+class __LoginRegisterButtonsState extends State<_LoginRegisterButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      direction: Axis.horizontal,
+      alignment: WrapAlignment.center,
+      spacing: 10,
+      runSpacing: 10,
+      children: [
+        RoundedButtonWithIcon(
+          onTap: () {},
+          width: .42.sw,
+          color: const Color(0xFF4267B3),
+          icon: CuidapetIcons.facebook,
+          label: 'Facebook',
+        ),
+        RoundedButtonWithIcon(
+          onTap: () {},
+          width: .42.sw,
+          color: const Color(0xFFE15031),
+          icon: CuidapetIcons.google,
+          label: 'Google',
+        ),
+        RoundedButtonWithIcon(
+          onTap: () {},
+          width: .42.sw,
+          color: context.primaryColor,
+          icon: Icons.mail,
+          label: 'Cadastre-se',
+        ),
+      ],
+    );
+  }
+}

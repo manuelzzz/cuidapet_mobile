9c32ce7175b4c1e70a1a13e28c43ceee4a65524b
manel <manuelss.23.bil@gmail.com>
2023-06-08 12:57:51 -0300

    Transforming the image logo into a widget

---
+++ lib/app/core/ui/widgets/cuidapet_logo_image.dart
@@ -0,0 +1,17 @@
+import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extension.dart';
+import 'package:flutter/material.dart';
+
+class CuidapetLogoImage extends StatelessWidget {
+  const CuidapetLogoImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        'images/logo.png',
+        width: 162.w,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+}

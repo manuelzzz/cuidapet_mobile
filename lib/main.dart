62b2bccbcdac4656105d7288bfa9ab43e12d35bc
Manel <manuelss.23.bil@gmail.com>
2023-06-09 14:58:16 -0300

    First configuration for firebase on project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:cuidapet_mobile/app/app_module.dart';
 import 'package:cuidapet_mobile/app/app_widget.dart';
+import 'package:cuidapet_mobile/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ModularApp(
       module: AppModule(),

947005f9ac053391cbb97ebea447707eeb60e406
Manel <manuelss.23.bil@gmail.com>
2023-06-14 08:15:56 -0300

    RestClient add for all modules

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,9 +1,12 @@
+import 'package:cuidapet_mobile/app/core/rest_client/dio/dio_rest_client.dart';
+import 'package:cuidapet_mobile/app/core/rest_client/rest_client.dart';
 import 'package:cuidapet_mobile/app/modules/core/auth/auth_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class CoreModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.lazySingleton((i) => AuthStore(), export: true,),
+        Bind.lazySingleton((i) => AuthStore(), export: true),
+        Bind.lazySingleton<RestClient>((i) => DioRestClient(), export: true),
       ];
 }

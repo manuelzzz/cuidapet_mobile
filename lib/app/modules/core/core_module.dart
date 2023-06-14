74f08ab4e44f893155ddf5cff1bc615478284f68
Manel <manuelss.23.bil@gmail.com>
2023-06-14 09:19:55 -0300

    Logger added on core module, actually works in all app

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,3 +1,5 @@
+import 'package:cuidapet_mobile/app/core/logger/app_logger.dart';
+import 'package:cuidapet_mobile/app/core/logger/logger_app_logger_impl.dart';
 import 'package:cuidapet_mobile/app/core/rest_client/dio/dio_rest_client.dart';
 import 'package:cuidapet_mobile/app/core/rest_client/rest_client.dart';
 import 'package:cuidapet_mobile/app/modules/core/auth/auth_store.dart';
@@ -6,7 +8,17 @@ import 'package:flutter_modular/flutter_modular.dart';
 class CoreModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.lazySingleton((i) => AuthStore(), export: true),
-        Bind.lazySingleton<RestClient>((i) => DioRestClient(), export: true),
+        Bind.lazySingleton(
+          (i) => AuthStore(),
+          export: true,
+        ),
+        Bind.lazySingleton<RestClient>(
+          (i) => DioRestClient(),
+          export: true,
+        ),
+        Bind.lazySingleton<AppLogger>(
+          (i) => LoggerAppLoggerImpl(),
+          export: true,
+        )
       ];
 }

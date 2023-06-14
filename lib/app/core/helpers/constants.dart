a74154c5c37205079b1177025f133ff43f6a4a95
Manel <manuelss.23.bil@gmail.com>
2023-06-14 08:15:26 -0300

    Constants for firebase and env usage

---
+++ lib/app/core/helpers/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  Constants._();
+
+  static const ENV_BASE_URL_KEY = 'base_url';
+  static const ENV_REST_CLIENT_CONNECTION_TIMEOUT_KEY = 'rest_client_connection_timeout';
+  static const ENV_REST_CLIENT_RECEIVE_TIMEOUT_KEY = 'rest_client_receive_timeout';
+}

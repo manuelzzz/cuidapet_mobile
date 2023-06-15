9985b358d3a147898cbc8a617d126e8159909935
Manel <manuelss.23.bil@gmail.com>
2023-06-14 21:03:11 -0300

    LocalStorage implementations at core module. Actually localStorage acessible at all app

--- lib/app/modules/core/core_module.dart
+++ lib/app/modules/core/core_module.dart
@@ -1,3 +1,6 @@
+import 'package:cuidapet_mobile/app/core/local_storage/flutter_secure_storage/flutter_secure_storage_local_storage_impl.dart';
+import 'package:cuidapet_mobile/app/core/local_storage/local_storage.dart';
+import 'package:cuidapet_mobile/app/core/local_storage/shared_preferences/shared_preferences_local_storage_impl.dart';
 import 'package:cuidapet_mobile/app/core/logger/app_logger.dart';
 import 'package:cuidapet_mobile/app/core/logger/logger_app_logger_impl.dart';
 import 'package:cuidapet_mobile/app/core/rest_client/dio/dio_rest_client.dart';
@@ -19,6 +22,14 @@ class CoreModule extends Module {
         Bind.lazySingleton<AppLogger>(
           (i) => LoggerAppLoggerImpl(),
           export: true,
-        )
+        ),
+        Bind.lazySingleton<LocalStorage>(
+          (i) => SharedPreferencesLocalStorageImpl(),
+          export: true,
+        ),
+        Bind.lazySingleton<LocalSecureStorage>(
+          (i) => FlutterSecureStorageLocalStorageImpl(),
+          export: true,
+        ),
       ];
 }
